483ae6a080cf1e5283a89815f7035ff96aa95820
Jbohn44 <ejbohnert@yahoo.com>
2020-05-22 12:30:40 -0500

    added measurement model

---
+++ DomainModels/Models/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using DomainModels.Enums;
+using DomainModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModels.Models
+{
+    public class Measurement : IMeasurement
+    {
+        public int MeasurementId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double MeasurementValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public MeasurementType MeasurementType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
