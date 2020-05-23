5637ae8ba3ee18dac7c2b7cd54e32bf2b258b686
Jbohn44 <ejbohnert@yahoo.com>
2020-05-23 15:12:59 -0500

    Worked on data models

---
+++ Data/Models/Measurement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Data.Models
+{
+    public class Measurement
+    {
+        [Key]
+        public int MeasurementId { get; set; }
+        public int IngredientId { get; set; }
+        public int MeasurementTypeId { get; set; }
+        public double MeasurementValue { get; set; }
+    }
+}
