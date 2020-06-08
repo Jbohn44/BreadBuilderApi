16436d3c59cce988aa9dc862a353865ebd3db056
Jbohn44 <ejbohnert@yahoo.com>
2020-06-08 16:46:41 -0500

    Added formula repository and added interfaces

---
+++ Repository/Interfaces/IForumulaInstructionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using DomainModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IForumulaInstructionsRepository
+    {
+        public Task<List<IFormulaInstructions>> GetFormulaInstructions(int FormulaId);
+        public Task<IFormulaInstructions> Save(IFormulaInstructions instructions);
+    }
+}
